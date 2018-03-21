f08ab7b2240f3680d2aa4382365883b366b19a4b
Jens Fischer <soth@gmx.net>
2018-03-21 14:59:50 +0100

    first REST call

--- README.md
+++ README.md
@@ -29,3 +29,26 @@ npm test
 
 For a detailed explanation on how things work, check out the [guide](http://vuejs-templates.github.io/webpack/) and [docs for vue-loader](http://vuejs.github.io/vue-loader).
 
+
+
+## Our steps:
+
+
+* add `express`
+```bash
+$ npm install express --save
++ express@4.16.3
+updated 1 package in 7.839s
+```
+
+* add `server.js`
+
+* add `"heroku-postbuild": "npm run build"` to `package.json` `scripts`
+
+* add `axios`
+
+```bash
+$ npm install axios --save
++ axios@0.18.0
+added 3 packages in 7.883s
+```
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -80,16 +80,42 @@
         </a>
       </li>
     </ul>
+
+    <h1>Products</h1>
+
+    <ul v-if="products && products.length">
+      <li v-for="product of products" v-bind:key="product._id">
+        <p><strong>{{product.name}}</strong></p>
+        <p>{{product.sku}}</p>
+      </li>
+    </ul>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
+
+import axios from 'axios'
+
 export default {
   name: 'HelloWorld',
   data () {
     return {
+      products: [],
+      errors: [],
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+
+  created () {
+    axios.get('https://taggle.beyondshop.cloud/api/product-view/products/search/find-by-tags?tag=kitchen')
+    .then(response => {
+      console.log(response.data._embedded.products)
+      this.products = response.data._embedded.products
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
   }
 }
 </script>
