e28d925a7606ea0a6d639dd103a9a64da2ddf001
pk762 <pkalashnikov@epages.com>
2018-03-22 14:59:50 +0100

    home page added

---
+++ src/components/Home.vue
@@ -0,0 +1,58 @@
+<template>
+  <div class="hello">
+     <input autocomplete="off" autocorrect="off" autocapitalize="off" spellcheck="false"
+        v-model="shopname"
+        value="shopname"
+        v-on:keyup.enter="goToUrl"
+        autofocus
+        >
+     <p>To see the magik press #Enter</p>
+    </div>
+</template>
+
+<script>
+/* eslint-disable */
+
+export default {
+  name: 'Home',
+  data: function() {
+    
+    return {
+      shopname: 'taggle'
+    }
+  },
+  
+  methods: { 
+    goToUrl: function() {
+      this.$router.push({ path: '/' + this.shopname })
+    }
+  }
+}
+</script>
+
+
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h1, h2 {
+  font-weight: normal;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 10px;
+}
+a {
+  color: #42b983;
+}
+input {
+  width: 500px;
+  height: 50px;
+  text-align: center;
+  font-size: 150%;
+  margin-top: 20px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Home from '@/components/Home'
 import Tags from '@/components/Tags'
 import ProductList from '@/components/ProductList'
 import ProductDetails from '@/components/ProductDetails'
@@ -11,8 +11,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Home',
+      component: Home
     }, {
       path: '/:shop',
       name: 'Tags',
